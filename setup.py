ef335f1bd2141af085297ae9b3a2a295d72b0122
youngkyunjung <younginpiniti@gmail.com>
2023-11-18 09:07:49 +0900

    배포하기 위한 작업

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name ='inpinitiFinance',
+    version = '1.0.0',
+    description = 'finance data',
+    author = 'inpiniti',
+    author_email = 'younginpiniti@gmail.com',
+    url = None,
+    py_modules = ['ifinance']
+)
