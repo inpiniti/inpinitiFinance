c22438fe984b22b0c994092292f3b8e4b3a3d32d
youngkyunjung <younginpiniti@gmail.com>
2023-11-18 23:09:12 +0900

    버전 변경

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name ='inpinitiFinance',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'finance data',
     author = 'inpiniti',
     author_email = 'younginpiniti@gmail.com',
